f9a3c98575a2cb1ebee66e8a7a1f434a60b72990
Jonathan Eisenhamer <eisenhamer@stsci.edu>
2021-02-01 15:27:07 -0500

    Add test for context management and open file management

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -8,6 +8,25 @@ from models import FitsModel
 DATA_PATH = Path(__file__).parent / 'data'
 
 
+def test_context_management(tmp_path):
+    """Ensure working across context management"""
+    new_model_filename = tmp_path / 'new_model.fits'
+
+    with FitsModel(DATA_PATH / 'fits_model.fits') as model:
+        new_model = FitsModel(model)
+        assert len(new_model._models_using_resources) == 2
+
+    assert len(new_model._models_using_resources) == 1
+    new_model.save(new_model_filename)
+
+    # Success is no exceptions and the file exists
+    assert new_model_filename.exists()
+
+    # Close up.
+    new_model.close()
+    assert len(new_model._models_using_resources) == 0
+
+
 def test_reinstantiation(tmp_path):
     """Ensure file resources exist after re-instantiation
 
@@ -20,8 +39,15 @@ def test_reinstantiation(tmp_path):
 
     model = FitsModel(DATA_PATH / 'fits_model.fits')
     new_model = FitsModel(model)
+    assert len(new_model._models_using_resources) == 2
+
     model.close()
-    new_model.save(new_model_filename)
+    assert len(new_model._models_using_resources) == 1
 
     # Success is no exceptions and the file exists
+    new_model.save(new_model_filename)
     assert new_model_filename.exists()
+
+    # Close up.
+    new_model.close()
+    assert len(new_model._models_using_resources) == 0
