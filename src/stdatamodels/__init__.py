381dac69745fa9bbbc98f5d40c74619dc9b6a7ca
Tyler Pauly <tapastro@gmail.com>
2022-07-28 14:49:46 -0400

    add to init for easier import?

--- src/stdatamodels/__init__.py
+++ src/stdatamodels/__init__.py
@@ -1,8 +1,8 @@
-from .model_base import DataModel
+from .model_base import DataModel, ModelList
 from . import _version
 
 
-__all__ = ['DataModel', '__version__']
+__all__ = ['DataModel', 'ModelList', '__version__']
 
 
 __version__ = _version.version
