f628f5ea033afb46d934ffa4be7f9a91568503f8
William Jamieson <wjamieson@stsci.edu>
2023-01-19 15:48:09 -0500

    Create a module for jwst
    Update pytest configuration to discover the new datamodels tests
    
    Package schemas, create an entrypoint for them, add `stcal` dependency

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 from setuptools import setup
 
-setup(use_scm_version={"write_to": "src/stdatamodels/_version.py"})
+package_data = {
+    "": [
+        "*.yaml",
+    ]
+}
+
+setup(
+    use_scm_version={"write_to": "src/stdatamodels/_version.py"},
+    package_data=package_data,
+)
