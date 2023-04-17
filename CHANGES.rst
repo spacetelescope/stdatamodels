0e37e05c095eb168d5390df17ff3c043d2fbb0f8
William Jamieson <wjamieson@stsci.edu>
2023-04-17 11:46:05 -0400

    Remove `s3_utils` module (#154)
    * Remove `s3_utils` and all its references
    
    * Remove unneed `aws` install option
    
    * Change stpipe version for jwst tox tests

--- CHANGES.rst
+++ CHANGES.rst
@@ -3,8 +3,11 @@
 
 - Add pixel replacement step keyword to jwst.datamodels core schema, and change
   DQ bit 28 from ``UNRELIABLE_RESET`` to ``FLUX_ESTIMATED``. [#149]
+  
 - drop support for Python 3.8 [#143]
 
+- Remove the defunct ``s3_utils`` module, so that ``stpipe`` no longer needs to depend 
+  on this package. This also removes the ``aws`` install option as this is no longer need. [#154]
 
 1.3.1 (2023-03-31)
 ==================
--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,6 @@ import pytest
 
 import asdf
 
-import helpers
-from stdatamodels import s3_utils
-
 
 def pytest_addoption(parser):
     parser.addoption(
@@ -16,22 +13,6 @@ def pytest_addoption(parser):
         default=False,
         help="Skip tests against crds",
     )
-
-
-@pytest.fixture(autouse=True)
-def monkey_patch_s3_client(monkeypatch, request):
-    # If tmpdir is used in the test, then it is providing the file.  Map to it.
-    if "s3_root_dir" in request.fixturenames:
-        path = request.getfixturevalue("s3_root_dir")
-    else:
-        path = None
-    monkeypatch.setattr(s3_utils, "_CLIENT", helpers.MockS3Client(path))
-
-
-@pytest.fixture
-def s3_root_dir(tmpdir):
-    return tmpdir
-
 
 @pytest.fixture(scope="session", autouse=True)
 def register_schemas():
