1398bc3f4e4d212e395bdaed98f1f48e76d0776e
Nadia Dencheva <nadia.astropy@gmail.com>
2023-04-27 11:22:15 -0400

    Update src/stdatamodels/jwst/datamodels/model_base.py
    Co-authored-by: Brett Graham <brettgraham@gmail.com>

--- src/stdatamodels/jwst/datamodels/model_base.py
+++ src/stdatamodels/jwst/datamodels/model_base.py
@@ -56,9 +56,5 @@ class JwstDataModel(_DataModel):
 
 
 class DataModel(JwstDataModel):
-
-    def __init__(self):
-        super(JwstDataModel, self).__init__()
-
     def __init_subclass__(cls):
         warnings.warn("Class DataModel has been renamed JwstDataModel. It will be removed in v 1.5.", DeprecationWarning, 2)
