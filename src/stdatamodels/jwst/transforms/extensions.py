f294d8a609e58662720dd8ad5d5ab0141fc85b83
Brett <brettgraham@gmail.com>
2023-07-17 18:13:41 -0400

    remove unused V23ToSkyConverter from jwst.transforms extension 1.0.0
    V23ToSkyConverter is supported in the jwst.transforms asdf extension
    version 0.7.0 (see 0.7.0 manifest) but is not currently supported in
    1.0.0 (see 1.0.0 manifest)
    
    This removes V23ToSkyConverter from the 1.0.0 extension. Previously
    asdf ignored any Converter that lists tags that are not in the
    manifest. Newer versions of asdf will produce errors for Converters
    that don't support any supported tags (one's listed in the manifest)
    and implement a select_tag method (which this one does by inheriting
    from Converter).

--- src/stdatamodels/jwst/transforms/extensions.py
+++ src/stdatamodels/jwst/transforms/extensions.py
@@ -8,35 +8,44 @@ from .converters.jwst_models import (Gwa2SlitConverter, Slit2MsaConverter, Logic
                                      CoordsConverter, V23ToSkyConverter)
 
 
-JWST_TRANSFORM_CONVERTERS = [
-    CoordsConverter(),
-    Gwa2SlitConverter(),
-    Slit2MsaConverter(),
-    LogicalConverter(),
-    NirissSOSSConverter(),
-    RefractionIndexConverter(),
-    Rotation3DToGWAConverter(),
-    MIRI_AB2SliceConverter(),
-    NIRCAMGrismDispersionConverter(),
-    NIRISSGrismDispersionConverter(),
-    GratingEquationConverter(),
-    SnellConverter(),
-    V23ToSkyConverter(),
-]
-
 # The order here is important; asdf will prefer to use extensions
 # that occur earlier in the list.
-TRANSFORM_MANIFEST_URIS = [
-    "asdf://stsci.edu/jwst_pipeline/manifests/jwst_transforms-1.0.0",
-    "asdf://stsci.edu/jwst_pipeline/manifests/jwst_transforms-0.7.0"
-]
-
-
 TRANSFORM_EXTENSIONS = [
     ManifestExtension.from_uri(
-        uri,
+        "asdf://stsci.edu/jwst_pipeline/manifests/jwst_transforms-1.0.0",
         legacy_class_names=["jwst.transforms.jwextension.JWSTExtension"],
-        converters=JWST_TRANSFORM_CONVERTERS,
+        converters=[
+            CoordsConverter(),
+            Gwa2SlitConverter(),
+            Slit2MsaConverter(),
+            LogicalConverter(),
+            NirissSOSSConverter(),
+            RefractionIndexConverter(),
+            Rotation3DToGWAConverter(),
+            MIRI_AB2SliceConverter(),
+            NIRCAMGrismDispersionConverter(),
+            NIRISSGrismDispersionConverter(),
+            GratingEquationConverter(),
+            SnellConverter(),
+        ],
+    ),
+    ManifestExtension.from_uri(
+        "asdf://stsci.edu/jwst_pipeline/manifests/jwst_transforms-0.7.0",
+        legacy_class_names=["jwst.transforms.jwextension.JWSTExtension"],
+        converters=[
+            CoordsConverter(),
+            Gwa2SlitConverter(),
+            Slit2MsaConverter(),
+            LogicalConverter(),
+            NirissSOSSConverter(),
+            RefractionIndexConverter(),
+            Rotation3DToGWAConverter(),
+            MIRI_AB2SliceConverter(),
+            NIRCAMGrismDispersionConverter(),
+            NIRISSGrismDispersionConverter(),
+            GratingEquationConverter(),
+            SnellConverter(),
+            V23ToSkyConverter(),
+        ],
     )
-    for uri in TRANSFORM_MANIFEST_URIS
 ]
