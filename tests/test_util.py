2abbb189b9a16e727efec04108c70f149a2d8809
Brett <brettgraham@gmail.com>
2023-08-04 11:41:18 -0400

    move test from test_storage to test_util

--- tests/test_util.py
+++ tests/test_util.py
@@ -11,6 +11,18 @@ from asdf.tags.core import Software, HistoryEntry
 from stdatamodels import util
 
 
+def test_gentle_asarray():
+    x = np.array([('abc', 1.0)], dtype=[
+        ('FOO', 'S3'),
+        ('BAR', '>f8')])
+
+    new_dtype = [('foo', '|S3'), ('bar', '<f8')]
+
+    y = util.gentle_asarray(x, new_dtype)
+
+    assert y['bar'][0] == 1.0
+
+
 def test_gentle_asarray_array_input():
     """
     Test gentle_asarray with various forms of np.ndarray input,
