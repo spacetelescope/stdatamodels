1e2628d9084bcdf1272c6279425052481fefe565
Maria Pena-Guerrero <pena@stsci.edu>
2023-10-09 10:58:38 -0400

    creating emicorr datamodel and schema

--- src/stdatamodels/jwst/datamodels/emi.py
+++ src/stdatamodels/jwst/datamodels/emi.py
@@ -1,4 +1,6 @@
 from .reference import ReferenceFileModel
+from stdatamodels.dynamicdq import dynamic_mask
+from .dqflags import pixel
 
 
 __all__ = ['EmiModel']
@@ -10,10 +12,25 @@ class EmiModel(ReferenceFileModel):
 
     Parameters
     __________
-    rfc_freq_short : numpy table
-    rfc_freq_medium : numpy table
-    rfc_freq_long : numpy table
-    max_amp : numpy table
+    data : numpy float32 array
+         The science data
+
+    dq : numpy uint32 array
+         Data quality array
+
+    err : numpy float32 array
+         Error array
+
+    dq_def : numpy table
+         DQ flag definitions
     """
     schema_url = "http://stsci.edu/schemas/jwst_datamodel/emi.schema"
+
+    def __init__(self, init=None, **kwargs):
+        super(EmiModel, self).__init__(init=init, **kwargs)
+
+        self.dq = dynamic_mask(self, pixel)
+
+        # Implicitly create arrays
+        self.err = self.err
 
