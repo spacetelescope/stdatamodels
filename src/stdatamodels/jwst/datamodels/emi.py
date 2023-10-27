335bcb52671a068ce3111cdeda5a3952ae014308
Maria Pena-Guerrero <pena@stsci.edu>
2023-10-27 17:41:11 -0400

    changing from fits to asdf

--- src/stdatamodels/jwst/datamodels/emi.py
+++ src/stdatamodels/jwst/datamodels/emi.py
@@ -11,12 +11,16 @@ class EmiModel(ReferenceFileModel):
 
     Parameters
     __________
-    data : numpy float32 array
-         The science data
+    data : numpy table
+         The reference waves to correct for MIRI EMI.
+        A table-like object containing phase amplitude values
+        corresponding to the appropriate frequency
+        - Hz390: float32 1D array
+        - Hz218a: float32 1D array
+        - Hz218b: float32 1D array
+        - Hz218c: float32 1D array
+        - Hz164: float32 1D array
+        - Hz10: float32 1D array
     """
     schema_url = "http://stsci.edu/schemas/jwst_datamodel/emi.schema"
-
-    def __init__(self, init=None, **kwargs):
-        super(EmiModel, self).__init__(init=init, **kwargs)
-
 
