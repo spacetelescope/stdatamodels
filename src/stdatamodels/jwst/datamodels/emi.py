6658b33f3609c6190aa3c0fe3f02014785a430ca
Maria Pena-Guerrero <pena@stsci.edu>
2023-11-29 13:31:42 -0500

    modified schema to be as generic as possible

--- src/stdatamodels/jwst/datamodels/emi.py
+++ src/stdatamodels/jwst/datamodels/emi.py
@@ -22,3 +22,15 @@ class EmiModel(ReferenceFileModel):
         - Hz10: float32 1D array
     """
     schema_url = "http://stsci.edu/schemas/jwst_datamodel/emi.schema"
+    reftype = "emicorr"
+
+    def __init__(self, init=None, **kwargs):
+        super(EmiModel, self).__init__(init=init, **kwargs)
+
+    def on_save(self, path=None):
+        self.meta.reftype = self.reftype
+        self.meta.instrument.name = "MIRI"
+
+    def validate(self):
+        super(EmiModel, self).validate()
+
