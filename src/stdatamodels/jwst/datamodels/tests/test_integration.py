742c21e9a96041eb5a7bf898a9be0ce2718804bb
Brett <brettgraham@gmail.com>
2024-02-02 12:00:52 -0500

    add test that tag and schema versions match in transform manifests

--- src/stdatamodels/jwst/datamodels/tests/test_integration.py
+++ src/stdatamodels/jwst/datamodels/tests/test_integration.py
@@ -52,3 +52,19 @@ def test_resource_id(resource):
     assert (
         resource_manager[schema["id"]] == contents
     ), f"id[{schema['id']}] for resource[{resource}] did not return the contents of the resource"
+
+
+@pytest.mark.parametrize("manifest_filename", TRANSFORM_MANIFESTS)
+def test_manifest_tag_versions(manifest_filename):
+    with open(manifest_filename, "rb") as f:
+        contents = f.read()
+    manifest = yaml.safe_load(contents.decode("ascii"))
+
+    for tag_def in manifest["tags"]:
+        tag_uri = tag_def["tag_uri"]
+        tag_name, tag_version = asdf.versioning.split_tag_version(tag_uri)
+        schema_uri = tag_def["schema_uri"]
+        schema_name, schema_version = asdf.versioning.split_tag_version(schema_uri)
+        # although not generally required for stdatamodels transforms all
+        # tag versions should match schema version
+        assert tag_version == schema_version
