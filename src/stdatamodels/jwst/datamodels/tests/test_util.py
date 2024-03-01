eb18ebab3868e0cace574d431fb7c0a73beccea2
Brett <brettgraham@gmail.com>
2024-03-01 14:10:51 -0500

    skip darwin test if not darwin

--- src/stdatamodels/jwst/datamodels/tests/test_util.py
+++ src/stdatamodels/jwst/datamodels/tests/test_util.py
@@ -1,3 +1,5 @@
+import platform
+
 from stdatamodels.jwst.datamodels import util
 
 import pytest
@@ -6,8 +8,13 @@ import pytest
 @pytest.mark.parametrize("func_name", [
     "get_available_memory",
     "get_available_memory_linux",
-    "get_available_memory_darwin",
 ])
 def test_deprecated(func_name):
     with pytest.warns(DeprecationWarning, match=f"{func_name} is deprecated"):
         getattr(util, func_name)()
+
+
+@pytest.mark.skipif(platform.system() != 'Darwin', reason="only runs on darwin")
+def test_deprecated_get_available_memory_darwin():
+    with pytest.warns(DeprecationWarning, match="get_available_memory_darwin is deprecated"):
+        util.get_available_memory_darwin()
