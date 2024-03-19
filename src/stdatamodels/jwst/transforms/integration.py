821bfb10a01d9f8481d5e1a661f7476892f2efdb
Brett Graham <brettgraham@gmail.com>
2024-03-19 09:27:58 -0400

    drop python 3.9 (#287)

--- src/stdatamodels/jwst/transforms/integration.py
+++ src/stdatamodels/jwst/transforms/integration.py
@@ -2,12 +2,7 @@
 This module supports the entry points for ASDF support for the `jwst.transforms`.
 """
 
-import sys
-
-if sys.version_info < (3, 9):
-    import importlib_resources
-else:
-    import importlib.resources as importlib_resources
+import importlib.resources
 
 
 from asdf.resource import DirectoryResourceMapping
@@ -26,7 +21,7 @@ def get_resource_mappings():
     list of the `asdf.resource.ResourceMapping` instances containing the `jwst.transforms`
     schemas.
     """
-    resources_root = importlib_resources.files(transforms) / "resources"
+    resources_root = importlib.resources.files(transforms) / "resources"
     if not resources_root.is_dir():
         raise RuntimeError(f"Missing resources directory: {resources_root=}")
 
