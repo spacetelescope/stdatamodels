eead7aed3dcd29b9577813f2cb806ca01312fd92
Tyler Pauly <tapastro@gmail.com>
2024-08-28 16:32:45 -0400

    fix incorrect import, add params

--- src/stdatamodels/jwst/datamodels/__init__.py
+++ src/stdatamodels/jwst/datamodels/__init__.py
@@ -66,7 +66,7 @@ from .rscd import RSCDModel
 from .saturation import SaturationModel
 from .segmap import SegmentationMapModel
 from .slit import SlitModel, SlitDataModel
-from .pastasossmodel import SossTraceModel
+from .pastasossmodel import PastasossModel
 from .sossextractmodel import SossExtractModel
 from .sosswavegrid import SossWaveGridModel
 from .spec import SpecModel
