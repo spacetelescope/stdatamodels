7798b8acb4c143d5ccc786d60626c305397b4b7c
Melanie Clarke <mclarke@stsci.edu>
2025-01-16 10:07:57 -0500

    JP-3790: MIRI MRS models to hold residual fringe correction  (#377)
    Co-authored-by: jemorrison <morrison@as.arizona.edu>

--- src/stdatamodels/jwst/datamodels/spec.py
+++ src/stdatamodels/jwst/datamodels/spec.py
@@ -1,7 +1,7 @@
 from .model_base import JwstDataModel
 
 
-__all__ = ['SpecModel']
+__all__ = ['SpecModel', 'MRSSpecModel']
 
 
 class SpecModel(JwstDataModel):
@@ -11,8 +11,23 @@ class SpecModel(JwstDataModel):
     Parameters
     __________
     spec_table : numpy table
-        Extracted spectral data table
+        Extracted spectral data table.
         A table with at least four columns:  wavelength, flux, an error
         estimate for the flux, and data quality flags.
     """
     schema_url = "http://stsci.edu/schemas/jwst_datamodel/spec.schema"
+
+
+class MRSSpecModel(SpecModel):
+    """
+    A data model for MIRI MRS 1D spectra with residual fringe corrections.
+
+    Parameters
+    __________
+    spec_table : numpy table
+        Extracted spectral data table.
+        A table with the standard spectral columns plus three extra
+        columns for residual fringe corrected data: flux, surface brightness,
+        and background.
+    """
+    schema_url = "http://stsci.edu/schemas/jwst_datamodel/mrs_spec.schema"
