0c9173200296460af83541254814390b58ec0330
Brett <brettgraham@gmail.com>
2025-01-31 14:51:02 -0500

    fix suggestion formatting

--- src/stdatamodels/dynamicdq.py
+++ src/stdatamodels/dynamicdq.py
@@ -33,7 +33,12 @@ def dynamic_mask(input_model, mnemonic_map, inv=False):
 
     dq_table = input_model.dq_def
     # Get the DQ array and the flag definitions
-    if (dq_table is not None) and (not np.isscalar(dq_table)) and (len(dq_table.shape)) and (len(dq_table)):
+    if (
+        (dq_table is not None)
+        and (not np.isscalar(dq_table))
+        and (len(dq_table.shape))
+        and (len(dq_table))
+    ):
         #
         # Make an empty mask
         dqmask = np.zeros(input_model.dq.shape, dtype=input_model.dq.dtype)
