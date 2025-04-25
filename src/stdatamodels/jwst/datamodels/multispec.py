d02c184b33ba9cd69b7ebfa170cb69e36413da65
Melanie Clarke <mclarke@stsci.edu>
2025-04-25 14:56:11 -0400

    TSO spec models

--- src/stdatamodels/jwst/datamodels/multispec.py
+++ src/stdatamodels/jwst/datamodels/multispec.py
@@ -1,13 +1,13 @@
 from .model_base import JwstDataModel
-from .spec import SpecModel, MRSSpecModel
+from .spec import SpecModel, MRSSpecModel, TSOSpecModel
 
 
-__all__ = ["MultiSpecModel", "MRSMultiSpecModel"]
+__all__ = ["MultiSpecModel", "MRSMultiSpecModel", "TSOMultiSpecModel"]
 
 
 class MultiSpecModel(JwstDataModel):
     """
-    A data model for multi-spec images.
+    A data model for multi-spec tables.
 
     This model has a special member `spec` that can be used to
     deal with an entire spectrum at a time.  It behaves like a list::
@@ -59,7 +59,7 @@ class MultiSpecModel(JwstDataModel):
 
 class MRSMultiSpecModel(JwstDataModel):
     """
-    A data model for MIRI MRS multi-spec images.
+    A data model for MIRI MRS multi-spec tables.
 
     This model has a special member `spec` that can be used to
     deal with an entire spectrum at a time.  It behaves identically
@@ -77,3 +77,33 @@ class MRSMultiSpecModel(JwstDataModel):
             return
 
         super(MRSMultiSpecModel, self).__init__(init=init, **kwargs)
+
+
+class TSOMultiSpecModel(JwstDataModel):
+    """
+    A data model for TSO multi-integration, multi-spectra tables.
+
+    This model has a special member `spec` that is used to contain spectra
+    from multiple integrations at a time.  It behaves identically
+    to `MultiSpecModel`, except that each row in the spectral
+    table corresponds to the full spectrum for a single integration,
+    so that all integrations are stored in the same EXTRACT1D
+    extension.  For the standard MultiSpecModel, column data is
+    one-dimensional for each extension.  For this model, column data is
+    two-dimensional.
+
+    In addition, the spectra for this model have additional columns
+    to contain the segment number and integration identifying the spectrum
+    in each row.
+    """
+
+    schema_url = "http://stsci.edu/schemas/jwst_datamodel/tso_multispec.schema"
+
+    def __init__(self, init=None, **kwargs):
+        if isinstance(init, TSOSpecModel):
+            super(TSOMultiSpecModel, self).__init__(init=None, **kwargs)
+            self.spec.append(self.spec.item())
+            self.spec[0].spec_table = init.spec_table
+            return
+
+        super(TSOMultiSpecModel, self).__init__(init=init, **kwargs)
--- src/stdatamodels/jwst/datamodels/spec.py
+++ src/stdatamodels/jwst/datamodels/spec.py
@@ -1,7 +1,7 @@
 from .model_base import JwstDataModel
 
 
-__all__ = ["SpecModel", "MRSSpecModel"]
+__all__ = ["SpecModel", "MRSSpecModel", "TSOSpecModel"]
 
 
 class SpecModel(JwstDataModel):
@@ -19,7 +19,7 @@ class SpecModel(JwstDataModel):
     schema_url = "http://stsci.edu/schemas/jwst_datamodel/spec.schema"
 
 
-class MRSSpecModel(SpecModel):
+class MRSSpecModel(JwstDataModel):
     """
     A data model for MIRI MRS 1D spectra with residual fringe corrections.
 
@@ -33,3 +33,20 @@ class MRSSpecModel(SpecModel):
     """
 
     schema_url = "http://stsci.edu/schemas/jwst_datamodel/mrs_spec.schema"
+
+
+class TSOSpecModel(JwstDataModel):
+    """
+    A data model for TSO 1D spectra with multiple integrations.
+
+    Attributes
+    ----------
+    spec_table : numpy table
+        Extracted spectral data table.
+        A table with the standard spectral columns plus two extra
+        columns for TSO data: segment, integration. Column data
+        is 2D for this model, with each row containing the spectrum
+        for a single integration.
+    """
+
+    schema_url = "http://stsci.edu/schemas/jwst_datamodel/tso_spec.schema"
