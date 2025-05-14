383f09b8b073ee1b7e68b2cb3c2bfa8ad9192be3
Ned Molter <emolter@stsci.edu>
2025-05-14 16:59:43 -0400

    Fix typos and add changelog entry

--- src/stdatamodels/jwst/datamodels/multispec.py
+++ src/stdatamodels/jwst/datamodels/multispec.py
@@ -81,7 +81,7 @@ class MRSMultiSpecModel(JwstDataModel):
 
 class WFSSMultiSpecModel(JwstDataModel):
     """
-    A data model for NIRSpec and NIRISS WFSS 1D spectra.
+    A data model for NIRCam andNIRISS WFSS 1D spectra.
 
     This model differs from the other MultiSpecModel classes in that
     it is designed to hold all the spectra from a single WFSS exposure
