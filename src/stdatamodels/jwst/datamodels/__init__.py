dc5661f8fee37bf3890c56463d94423c96ab2c0c
Ned Molter <emolter@users.noreply.github.com>
2025-06-10 15:41:24 -0700

    JP-3931: New data format for WFSS extracted spectra (#466)

--- src/stdatamodels/jwst/datamodels/__init__.py
+++ src/stdatamodels/jwst/datamodels/__init__.py
@@ -13,7 +13,7 @@ from .apcorr import NrsMosApcorrModel, NrsIfuApcorrModel, NrsFsApcorrModel
 from .asn import AsnModel
 from .background import SossBkgModel, WfssBkgModel
 from .barshadow import BarshadowModel
-from .combinedspec import CombinedSpecModel
+from .combinedspec import CombinedSpecModel, WFSSCombinedSpecModel
 from .contrast import ContrastModel
 from .cube import CubeModel
 from .dark import DarkModel
@@ -37,10 +37,10 @@ from .level1b import Level1bModel
 from .linearity import LinearityModel
 from .mask import MaskModel
 from .mrsxartcorr import MirMrsXArtCorrModel
-from .multicombinedspec import MultiCombinedSpecModel
+from .multicombinedspec import MultiCombinedSpecModel, WFSSMultiCombinedSpecModel
 from .multiexposure import MultiExposureModel
 from .multislit import MultiSlitModel
-from .multispec import MultiSpecModel, MRSMultiSpecModel, TSOMultiSpecModel
+from .multispec import MultiSpecModel, MRSMultiSpecModel, TSOMultiSpecModel, WFSSMultiSpecModel
 from .nirspec_flat import NirspecFlatModel, NirspecQuadFlatModel
 from .nrm import NRMModel
 from .outlierifuoutput import OutlierIFUOutputModel
@@ -78,7 +78,7 @@ from .slit import SlitModel, SlitDataModel
 from .pastasossmodel import PastasossModel
 from .sossextractmodel import SossExtractModel
 from .sosswavegrid import SossWaveGridModel
-from .spec import SpecModel, MRSSpecModel, TSOSpecModel
+from .spec import SpecModel, MRSSpecModel, TSOSpecModel, WFSSSpecModel
 from .speckernel import SpecKernelModel
 from .specprofile import SpecProfileModel, SpecProfileSingleModel
 from .specpsf import SpecPsfModel
@@ -252,6 +252,10 @@ __all__ = [
     "WaveMapModel",
     "WaveMapSingleModel",
     "WfssBkgModel",
+    "WFSSCombinedSpecModel",
+    "WFSSMultiCombinedSpecModel",
+    "WFSSMultiSpecModel",
+    "WFSSSpecModel",
 ]
 
 
