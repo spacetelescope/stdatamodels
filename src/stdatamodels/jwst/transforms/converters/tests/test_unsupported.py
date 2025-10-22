b6f09212209d6fba734185571ce10cffde304de6
Ned Molter <emolter@stsci.edu>
2025-10-22 10:26:12 -0400

    update test and changelog

--- src/stdatamodels/jwst/transforms/converters/tests/test_unsupported.py
+++ src/stdatamodels/jwst/transforms/converters/tests/test_unsupported.py
@@ -1,4 +1,6 @@
+import asdf
 import pytest
+from asdf.exceptions import AsdfConversionWarning
 
 import stdatamodels.jwst.datamodels as dm
 from stdatamodels.jwst.transforms.converters.jwst_models import UnsupportedConverterError
@@ -13,6 +15,8 @@ def model_with_unsupported_transform(tmp_path):
 %YAML 1.1
 %TAG ! tag:stsci.edu:asdf/
 --- !core/asdf-1.1.0
+meta:
+  model_type: JwstDataModel
 model: !<tag:stsci.edu:jwst_pipeline/v23tosky-0.7.0>
   angles: [-0.0193, -0.1432, -0.04, -65.60, 273.089]
   axes_order: zyxyz
@@ -23,9 +27,20 @@ model: !<tag:stsci.edu:jwst_pipeline/v23tosky-0.7.0>
     return path
 
 
-def test_load_unsupported_transform(model_with_unsupported_transform):
+def test_load_unsupported(model_with_unsupported_transform):
     """Test that loading a file with an unsupported transform raises the expected error."""
     with pytest.raises(
         UnsupportedConverterError,
     ):
         dm.open(model_with_unsupported_transform)
+
+
+def test_load_unsupported_with_flag(model_with_unsupported_transform):
+    """Test loading same file with the warning flag set raises a warning instead."""
+    asdf.get_config().warn_on_failed_conversion = True
+    with pytest.warns(AsdfConversionWarning):
+        model = dm.open(model_with_unsupported_transform)
+
+    # check that the transform is a serialized form of its inputs
+    assert model.model.angles == [-0.0193, -0.1432, -0.04, -65.60, 273.089]
+    assert model.model.axes_order == "zyxyz"
