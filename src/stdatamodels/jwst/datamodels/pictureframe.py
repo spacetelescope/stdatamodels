5907fe8e61789b9aad09657b61c3235b8c66c2fa
Melanie Clarke <mclarke@stsci.edu>
2025-10-27 15:38:54 -0400

    Remove unneeded ERR and DQ arrays from model

--- src/stdatamodels/jwst/datamodels/pictureframe.py
+++ src/stdatamodels/jwst/datamodels/pictureframe.py
@@ -1,6 +1,3 @@
-from stdatamodels.dynamicdq import dynamic_mask
-
-from .dqflags import pixel
 from .reference import ReferenceFileModel
 
 __all__ = ["PictureFrameModel"]
@@ -13,18 +10,10 @@ class PictureFrameModel(ReferenceFileModel):
     Attributes
     ----------
     data : numpy float32 array
-        The science data
-    dq : numpy uint32 array
-        Data quality array
-    err : numpy float32 array
-        Error array
-    dq_def : numpy table
-        DQ flag definitions
+        Picture frame slope data.
     """
 
     schema_url = "http://stsci.edu/schemas/jwst_datamodel/pictureframe.schema"
 
     def __init__(self, init=None, **kwargs):
         super(PictureFrameModel, self).__init__(init=init, **kwargs)
-
-        self.dq = dynamic_mask(self, pixel)
