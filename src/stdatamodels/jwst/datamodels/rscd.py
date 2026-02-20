dc167b00b9bee4ccc27f8a1788e98232033d403b
jemorrison <morrison@as.arizona.edu>
2026-02-20 13:46:39 -0700

    update rscd model for backwards compatiblity

--- src/stdatamodels/jwst/datamodels/rscd.py
+++ src/stdatamodels/jwst/datamodels/rscd.py
@@ -1,6 +1,5 @@
 import numpy as np
 from astropy.io import fits
-from numpy.lib.recfunctions import append_fields
 
 from .reference import ReferenceFileModel
 
@@ -18,19 +17,32 @@ def _migrate_rscd_table(hdulist):
 
     for ext in hdulist:
         # RSCD tables are usually in the first BinTableHDU
-        if ext.data is None or not isinstance(ext, fits.BinTableHDU):
-            continue
+        if ext.name == "RSCD_GROUP_SKIP" and isinstance(ext, fits.BinTableHDU):
+            if ext.data is not None and len(ext.data) > 0:
+                table_data = ext.data
+                # 1. check if the column exist
+                if "group_skip1" not in table_data.dtype.names:
+                    # Create the missing column.
+                    # Defaulting to -1. If the JWST RSCD step see -1 it will issue
+                    # a warning that an old rscd file is being used and set the
+                    # the value to 1.
 
-        table_data = ext.data
-        if "group_skip1" not in table_data.dtype.names:
-            # Create the missing column.
-            # Defaulting to 1 as per the baseline requirement.
-            new_col = np.full(table_data.shape[0], 0, dtype=">i4")
+                    new_col = fits.Column(
+                        name="group_skip1",
+                        format="J",
+                        array=np.full(len(ext.data), -1, dtype=">i4"),
+                    )
 
-            # This returns a new structured array with the column attached
-            ext.data = append_fields(
-                table_data, "group_skip1", new_col, usemask=False, asrecarray=True
-            )
+                    # 2. Create a new HDU by merging existing columns with the new one
+                    new_cols = fits.ColDefs(ext.columns) + fits.ColDefs([new_col])
+
+                    # 3. Create a temporary HDU to extract the correct data and header
+                    new_hdu = fits.BinTableHDU.from_columns(new_cols)
+
+                    # Update the existing extension's data and header
+                    ext.data = new_hdu.data
+                    ext.header = new_hdu.header
+                    ext.header["EXTNAME"] = "RSCD_GROUP_SKIP"
 
     return hdulist
 
