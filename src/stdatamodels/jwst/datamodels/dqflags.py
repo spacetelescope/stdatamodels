4d9365d03913609aab3f49bba8f6bd3402e3521c
Ned Molter <emolter@stsci.edu>
2026-03-27 14:50:14 -0400

    Remove unnecessary import in stdatamodels.jwst.datamodels.dqflags

--- src/stdatamodels/jwst/datamodels/dqflags.py
+++ src/stdatamodels/jwst/datamodels/dqflags.py
@@ -16,7 +16,6 @@ the formula `2**bit_number` where `bit_number` is the 0-index bit of interest.
 
 # These imports are here for backwards compatibility
 from astropy.nddata.bitmask import ap_interpret_bit_flags
-from astropy.nddata.bitmask import interpret_bit_flags as interpret_bit_flags
 
 from stdatamodels.basic_utils import multiple_replace
 from stdatamodels.dqflags import dqflags_to_mnemonics
@@ -75,7 +74,6 @@ __all__ = [
     "ap_interpret_bit_flags",
     "dqflags_to_mnemonics",
     "group",
-    "interpret_bit_flags",
     "multiple_replace",
     "pixel",
 ]
