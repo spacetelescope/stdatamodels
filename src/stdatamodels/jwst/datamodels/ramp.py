758be1c5bfc3f9a7df3bf7675d34ea365a201d13
Melanie Clarke <mclarke@stsci.edu>
2026-04-10 11:06:23 -0400

    Add superstripe ramp model

--- src/stdatamodels/jwst/datamodels/ramp.py
+++ src/stdatamodels/jwst/datamodels/ramp.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from .model_base import JwstDataModel
 
-__all__ = ["RampModel"]
+__all__ = ["RampModel", "SuperstripeRampModel"]
 
 
 class RampModel(JwstDataModel):
@@ -44,8 +44,6 @@ class RampModel(JwstDataModel):
         """
         Retrieve the schema-defined default value of an attribute.
 
-        Overrides the parent method to set pixeldq to a 2D array if
-        data is defined.
         Override the parent method to give zeroframe the correct shape
         if data is defined.
 
@@ -65,16 +63,85 @@ class RampModel(JwstDataModel):
         AttributeError
             If the given attribute is not defined in the schema.
         """
-        # This is a band-aid solution to allow multistripe modes to
-        # have 3D pixeldq, but set the default appropriately for
-        # all other RampModels.
-        # If we can move multi-stripe RampModels to their own model
-        # type, we can resume using the schema to make a default pixeldq.
-        if attr == "pixeldq" and self.data is not None:
-            return np.zeros(self.data.shape[-2:], dtype=np.uint32)
-
         if attr == "zeroframe" and self.data is not None:
             shp = (self.data.shape[0],) + self.data.shape[-2:]
             return np.zeros(shp, dtype=np.float32)
+
+        return super().get_default(attr)
+
+
+class SuperstripeRampModel(JwstDataModel):
+    """
+    A data model for 4D ramps taken in superstripe mode.
+
+    Attributes
+    ----------
+    data : numpy float32 array
+         The science data with dimensions ``(nint * nstripe, ngroup, ny, nx)``.
+    pixeldq : numpy uint32 array
+         3-D data quality array for all planes with dimensions ``(nstripe, ny, nx)``.
+    groupdq : numpy uint8 array
+         4-D data quality array for each plane, matching data dimensions.
+    zeroframe : numpy float32 array
+         3-D zeroframe array with dimensions ``(nint * nstripe, ny, nx)``.
+    group : numpy table
+         group parameters table
+    int_times : numpy table
+         table of times for each integration
+    """
+
+    schema_url = "http://stsci.edu/schemas/jwst_datamodel/superstripe_ramp.schema"
+
+    def __init__(self, init=None, **kwargs):
+        super(SuperstripeRampModel, self).__init__(init=init, **kwargs)
+
+        # Ensure that if data exists, pixeldq and groupdq arrays exist
+        if self.data is None:
+            return
+        if self.pixeldq is None:
+            self.pixeldq = self.get_default("pixeldq")
+        if self.groupdq is None:
+            self.groupdq = self.get_default("groupdq")
+
+    def get_default(self, attr):
+        """
+        Retrieve the schema-defined default value of an attribute.
+
+        Override the parent method to set pixeldq to a 3D array if
+        data and ``meta.subarray.num_superstripe`` are defined.
+
+        Also override the parent method to give zeroframe the correct
+        shape if data is defined.
+
+        Parameters
+        ----------
+        attr : str
+            Attribute to set to its default value.
+
+        Returns
+        -------
+        object or None
+            The default value for the given attribute. If the attribute is schema-defined
+            but has no default value in the schema, this will return None.
+
+        Raises
+        ------
+        AttributeError
+            If the given attribute is not defined in the schema.
+        """
+        if self.data is not None:
+            image_shape = self.data.shape[-2:]
+            nstripe = self.meta.subarray.num_superstripe
+
+            if attr == "pixeldq":
+                if nstripe is not None and nstripe > 0:
+                    return np.zeros((nstripe, *image_shape), dtype=np.uint32)
+
+                # If num_superstripe is not defined or is 0, pixeldq defaults do
+                # not make sense for this model.
+                return None
+
+            if attr == "zeroframe":
+                return np.zeros((self.data.shape[0], *image_shape), dtype=np.float32)
 
         return super().get_default(attr)
