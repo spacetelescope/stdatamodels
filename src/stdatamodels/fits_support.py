df8e1309d25da38644865b372a2668c495aacafa
Melanie Clarke <mclarke@stsci.edu>
2026-05-27 17:40:45 -0400

    Restore sequence_index after loop
    Co-authored-by: Brett Graham <brettgraham@gmail.com>

--- src/stdatamodels/fits_support.py
+++ src/stdatamodels/fits_support.py
@@ -338,15 +338,12 @@ def _fits_item_recurse(fits_context, validator, items, instance, schema):
         return
 
     if validator.is_type(items, "object"):
+        initial_index = fits_context.sequence_index
         for index, item in enumerate(instance):
-            # Update the FITS sequence index if it is a list of objects,
-            # to support things like lists of slits or spectra. No need
-            # if it is just a simple list of items.
-            if validator.is_type(item, "object"):
-                fits_context.sequence_index = index
-
+            fits_context.sequence_index = index
             for error in validator.descend(item, items, path=index):
                 yield error
+        fits_context.sequence_index = initial_index
     else:
         # We don't do the index trick on "tuple validated" sequences
         for (index, item), subschema in zip(enumerate(instance), items, strict=False):
