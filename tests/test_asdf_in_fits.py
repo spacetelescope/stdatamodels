2022cce1117a6eeee9f0e3cee3c09d16827a04aa
Brett Graham <brettgraham@gmail.com>
2026-08-18 15:33:49 -0400

    fix and improve nameless hdu asdf_in_fits test (#799)

--- tests/test_asdf_in_fits.py
+++ tests/test_asdf_in_fits.py
@@ -198,9 +198,15 @@ def test_open_asdf_in_fits_hdu(saved_array_model, test_array):
 def test_non_named_hdus(tmp_path):
     fn = tmp_path / "test.fits"
     ff = fits.HDUList([fits.PrimaryHDU()] + [fits.ImageHDU([i]) for i in range(10)])
+    # give a few names so that the index of nameless hdus
+    # doesn't match the index of all hdus
+    ff[5].name = "FOO"
+    ff[5].ver = 1
+    ff[7].name = "FOO"
+    ff[7].ver = 2
     tree = {"hdus": [hdu.data for hdu in ff[1:]]}
     asdf_in_fits.write(fn, tree, ff)
 
     with asdf_in_fits.open(fn) as af:
-        for i, hdu in enumerate(tree["hdus"]):
-            assert hdu.data[0] == i
+        for i, hdu in enumerate(af.tree["hdus"]):
+            assert hdu[0] == i
