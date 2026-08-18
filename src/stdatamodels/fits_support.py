d609ee541e83259b7050934bbf5407ae707edd83
Ned Molter <emolter@stsci.edu>
2026-08-18 12:31:09 -0400

    add the or i back

--- src/stdatamodels/fits_support.py
+++ src/stdatamodels/fits_support.py
@@ -869,7 +869,7 @@ def from_fits_asdf(
 
 def _map_hdulist_to_arrays(hdulist, af):
     # hdulist[key] is very inefficient so pre-index hdus
-    hdu_index = {(h.name, h.ver): h for h in hdulist}
+    hdu_index = {(h.name, h.ver or i): h for i, h in enumerate(hdulist)}
 
     def callback(node):
         if (
