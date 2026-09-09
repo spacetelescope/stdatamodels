2a022f28a29d2b243f7ef244b528803a46d6d111
Ned Molter <emolter@stsci.edu>
2026-09-09 13:39:30 -0400

    Add same handling for old combinedspec as for old multispec

--- tests/jwst/datamodels/test_multispec.py
+++ tests/jwst/datamodels/test_multispec.py
@@ -1,9 +1,9 @@
 from astropy.io import fits
 
-from stdatamodels.jwst.datamodels import WFSSMultiSpecModel, WFSSSpecModel
+from stdatamodels.jwst.datamodels import WFSSCombinedSpecModel, WFSSMultiSpecModel, WFSSSpecModel
 
 
-def test_old_wfss_multispec(tmp_path):
+def test_legacy_wfss_multispec(tmp_path):
     """Generate WFSSMultiSpecModel without the new contam_flux and contam_surf_bright columns."""
     new_colnames = ["CONTAM_FLUX", "CONTAM_SURF_BRIGHT"]
 
@@ -53,3 +53,52 @@ def test_old_wfss_multispec(tmp_path):
                 tab = spec.spec_table
                 for name in new_colnames:
                     assert name in tab.columns.names
+
+
+def test_legacy_wfss_combinedspec(tmp_path):
+    """Generate WFSSCombinedSpecModel without the new contam_flux and contam_surf_bright columns."""
+    new_colnames = ["CONTAM_FLUX", "CONTAM_SURF_BRIGHT"]
+
+    # Create a np recarray with old-style columns
+    default_spectable = WFSSCombinedSpecModel().get_default("spec_table")
+
+    # make a WFSSCombinedSpecModel with a few extensions
+    fname = tmp_path / "wfss_combinedspec.fits"
+    with WFSSCombinedSpecModel() as model:
+        model.spec_table = default_spectable
+        model.save(fname)
+
+    # Create old-style hdu in astropy fits because datamodel won't allow
+    # assignment or save if the table has the wrong number of columns,
+    # regardless of any of the validation strictness flags
+    with fits.open(fname) as hdulist:
+        for ext in hdulist:
+            if ext.name == "COMBINE1D":
+                # delete two columns from the table
+                for name in new_colnames:
+                    table_data = ext.data
+                    # for both missing attributes, find the schema-defined index in the table
+                    # and add a NaN-filled column at that index
+                    idx = table_data.dtype.names.index(name)
+                    new_cols = fits.ColDefs(ext.columns[:idx]) + fits.ColDefs(
+                        ext.columns[idx + 1 :]
+                    )
+
+                    # 3. Create a temporary HDU to extract the correct data and header
+                    new_hdu = fits.BinTableHDU.from_columns(new_cols)
+
+                    # Update the existing extension's data and header
+                    ext.data = new_hdu.data
+                    ext.header.update(new_hdu.header)
+
+        # check that the setup worked: isn't empty but it doesn't contain the contam columns
+        for name in new_colnames:
+            assert name not in hdulist[1].data.columns.names
+        assert "FLUX" in hdulist[1].data.columns.names
+
+        # Now try to instantiate a WFSSCombinedSpecModel from this hdulist
+        # and check that the columns have been restored
+        with WFSSCombinedSpecModel(hdulist) as model:
+            tab = model.spec_table
+            for name in new_colnames:
+                assert name in tab.columns.names
